cc5e35f02d3a5cbca3de1707175a689999f598fd
Bashar <basharaljabi.dev@gmail.com>
2025-12-31 13:49:03 +0400

    Initial commit

--- api/[...path].js
+++ api/[...path].js
@@ -1,25 +1,27 @@
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
-	const path = req.query.path.join("/");
+	const pathArray = req.query.path || [];
+	const path = pathArray.join("/");
 	const url = `https://training.tamkeen-dev.com/termc/public/${path}`;
 
 	try {
-		const response = await fetch(url, {
+		const options = {
 			method: req.method,
-			headers: {
-				...req.headers,
-				host: new URL(url).host,
-			},
-			body:
-				req.method !== "GET" && req.method !== "HEAD"
-					? req.body
-					: undefined,
-		});
+			headers: { ...req.headers },
+		};
 
+		if (req.method !== "GET" && req.method !== "HEAD") {
+			const chunks = [];
+			for await (const chunk of req) chunks.push(chunk);
+			options.body = Buffer.concat(chunks);
+		}
+
+		const response = await fetch(url, options);
 		const data = await response.text();
 		res.status(response.status).send(data);
 	} catch (err) {
+		console.error(err);
 		res.status(500).send(err.message || "Server error");
 	}
 }
